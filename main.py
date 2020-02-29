c37844190fd35b7f2da8f79dd0ebf851e44babeb
SherlockZhang <XinZhang@Xins-MacBook-Pro.local>
2020-02-29 10:22:39 -0500

    add input.txt and delete test output file

--- main.py
+++ main.py
@@ -1,23 +1,34 @@
 #from sys import argv
 from src import api
-import argparse
+from sys import argv
 import json
 
 def main():
-    parser = argparse.ArgumentParser(description='A tutorial of argparse!')
-    parser.add_argument("--page_size")
-    parser.add_argument("--num_page")
-    parser.add_argument("--output")
-    args = parser.parse_args()
-    page_size = args.page_size
-    num_page = int(args.num_page)
-    output_file = args.output
+    #input file
+    input_fn = argv[1]
+    
+    #ouptut file
+    try:
+        output_file = argv[2]
+    except Exception:
+        output_file = "output"
+    
+    #read input argument
+    with open(input_fn, "r") as fh:
+        for line in fh:
+            line = line.strip('\n')
+            page_size, num_page = line.split(' ')
+    page_size = int(page_size)        
+    num_page = int(num_page)
+
+    #get nycdata from sodapy and write to output file
     for i in range(num_page):
             output_data=api.get_data(page_size,i) 
             with open(output_file, 'a') as outfile:
                 json.dump(output_data, outfile)
+                outfile.write("\n")
+
 
     
 if __name__ == '__main__':
     main()
-     #output = api.get_data(page_size,1)
